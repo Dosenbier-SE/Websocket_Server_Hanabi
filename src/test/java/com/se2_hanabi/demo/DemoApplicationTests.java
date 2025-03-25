0c77682819a8952cb400e5a20c352ea2e49d70b4
Gabriel Ranftler <gabriel.ranftler@gmx.at>
2025-03-25 22:50:46 +0100

    added unit test for demoapplication

--- src/test/java/com/se2_hanabi/demo/DemoApplicationTests.java
+++ src/test/java/com/se2_hanabi/demo/DemoApplicationTests.java
@@ -1,13 +1,20 @@
 package com.se2_hanabi.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	private ApplicationContext applicationContext;
+
 	@Test
 	void contextLoads() {
+		assertThat(applicationContext).isNotNull();
 	}
-
 }
